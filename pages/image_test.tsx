6467de03f6bb5e4ba25ac68e49d325a06c8aaf01
Pranab Islam <pfi203@nyu.edu>
2023-01-20 12:14:10 -0500

    testing basic page add with image

---
+++ pages/image_test.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Image from 'next/image';
+//import mypic from './id_photo.jpg';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Seeing image</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <div>
+      <Image
+        //src="/static/images/id_photo.jpg"
+        //src={mypic}
+        src="/id_photo.jpg"
+        alt="Picture of pranab"
+        width={350}
+        height={450}
+        //layout="fixed" // layout="fill", layout="intrinsic" 
+      />
+    </div>
+    </>
+  );
+}
+
